1b993d082f7521ad7731e9ec0bb78c6d515a7589
Maryam <mary.elf97@gmail.com>
2024-07-24 11:19:58 -0700

    add comments

--- workflow/scripts/amplicon_simulator_wrapper.py
+++ workflow/scripts/amplicon_simulator_wrapper.py
@@ -12,6 +12,13 @@ def run_amplicon_simulator():
     subprocess.run(command, check=True)
     
 def create_config_dict(sample_name,sample_path,primer_file,output_file,read_cnt,read_length,r,d,R,X,e):
+    """
+    Generates a config file based on user-defined parameters
+    
+    Parameters:
+    parameters : defined in argparser
+    config file (str): config dictionary
+    """
     config = {
         'samples': {
             'sample_name': str(sample_name),
@@ -37,6 +44,13 @@ def create_config_dict(sample_name,sample_path,primer_file,output_file,read_cnt,
     return config
 
 def write_yaml(config):
+    """
+    Writes config dictionary to a yaml formatted file.
+    
+    Parameters:
+    config : config dictionary
+    config file (str): yaml formatted config file
+    """
     with open("config.yaml", 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
 
@@ -56,7 +70,7 @@ def main():
     parser = argparse.ArgumentParser(description="Amplicon sequencing simulator with user-defined proportions")
     parser.add_argument("--samplenames", "-s", help="name of samples in order, separated by a comma")
     parser.add_argument("--samplepaths", "-sp", help="path of samples in order, separated by a comma")
-    parser.add_argument("--proportions", "-pr", help="sample proportions separated by a comma, 0.0-0.1")
+    parser.add_argument("--proportions", "-pr", help="sample proportions separated by a comma, 0.0-1.0")
     parser.add_argument("--primers", "-p", help="Path to primer bed file")
     parser.add_argument("--readscnt", "-n", help="Total number of reads to be generated")
     parser.add_argument("--readlength", "-l", help="Total number of reads to be generated", default=150)
@@ -68,27 +82,33 @@ def main():
     parser.add_argument("--output", "-o", help="Path to output file")
     
     args = parser.parse_args()
-    # the sequence used to create amplicons
+    # create a list of sample names, paths and proportions
     sample_names = str(args.samplenames).split(",")
     sample_paths = str(args.samplepaths).split(",")
     proportions = str(args.proportions).split(",")
     proportions = list(map(float, proportions))
-    if len(sample_names) != len(proportions) != len(sample_paths):
-        raise Exception("number of samples and proportions should match")
+    # error if the proportion counts do not match the sample counts
+    if len(sample_names) != len(proportions) != len(sample_paths) != len(sample_paths):
+        raise Exception("Number of samples and proportions should match!")
+    # error if proportions do not add up to 1.0
     if sum(proportions) != 1.0:
-        raise Exception("Sum of all proportions should equal to 1.0")
+        raise Exception("Sum of all proportions should equal to 1.0!")
+    # create read counts based on user-defined proportions
     read_cnts = [i * int(args.readscnt) for i in proportions]
+    # loop over each isolate and create reads
     for name,path,count in zip(sample_names,sample_paths, read_cnts):
         config_file = create_config_dict(name,path,args.primers,args.output,count,args.readlength,
                                      args.mutationrate,args.outerdistance,
                                      args.indelfraction,args.indelextended,
                                      args.baseerrorrate)
-        
+        # write config file for each run
         write_yaml(config=config_file)
+        # simulate reads based on config file
         run_amplicon_simulator()
+        # create paths to merge all reads after simualtion
         read_path = os.path.join(os.path.abspath(args.output),"results",name,"reads/reads_1.fastq")
         output_path = os.path.join(os.path.abspath(args.output),"results/merged_reads.fastq")
-        print(read_path,output_path)
+        # merge fastq files 
         merge_fastq_files(read_path,output_path)
     
     
